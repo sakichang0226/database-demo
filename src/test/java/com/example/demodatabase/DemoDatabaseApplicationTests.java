c0d87d72085a89d21911208ce68523b646d719e2
sakichang0226 <saki.develop0226@gmail.com>
2024-04-07 19:57:52 +0900

    プロジェクトの作成

---
+++ src/test/java/com/example/demodatabase/DemoDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
